5e0dcffe32d0e560b42813a0df2324aef9278e21
Machtelinck <subaruparia@gmail.com>
2022-11-30 14:16:26 +0100

    Update settings.py

--- japan/japan/settings.py
+++ japan/japan/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-##hr!7+xvw_eq&%+=zv%21q9k1yj)rt$juy2)fjbl53h$(zd9j'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
